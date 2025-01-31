8fe56931cfe28accb228c964c73240be706642e8
Ali Norouzifar <norouzifar.ali@gmail.com>
2025-01-31 09:58:46 +0100

    version updates

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="prolysis",
     license="CC BY-NC 4.0",
-    version="0.1.3",
+    version="0.1.4",
     packages=find_packages(),
     install_requires=[
     "automata-lib ==8.4.0",
